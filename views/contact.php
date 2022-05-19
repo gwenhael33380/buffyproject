807bc3ce2c5a1a534be081acd951ee57dc75cc3a
gwenhael33380 <le.corre.gwen.hael@gmail.com>
2022-05-19 22:28:40 +0200

    crud users ok + design formulaire d'inscription

--- views/contact.php
+++ views/contact.php
@@ -11,24 +11,40 @@ require __DIR__ . '/header.php';
             <div class="content-title-contact">
                 <h2 class="title-form-contact">Formulaire de contact</h2>
             </div>
-            <div class="content-form-contact">
-                <form action="<?=HOME_URL. 'requests/form_post.php'?>" method="post">
-                    <div class="field-contact">
-                        <label class="form-label"for="email">Email<span class="red">*</span></label>
-                        <input class="input-form" type="text" id="email" name="email" placeholder="email..."/>
+            <div class="flex-contact-form">
+                <div class="content_img-contact">
+                    <img class="img_contact" src="<?php echo HOME_URL . 'assets/img/src/source/img-page-contact'; ?>" alt="">
+                </div>
+
+
+
+                <div class="content-form-contact">
+                    <form action="<?=HOME_URL. 'requests/form_post.php'?>" method="post">
+                        <div class="field-contact">
+                            <label class="form-label"for="email">Email<span class="red">*</span></label>
+                            <input class="input-form" type="text" id="email" name="email" placeholder="email..."/>
+                        </div>
+
+                        <div class="field-contact">
+                            <label class="form-label" for="subject">Sujet <span class="red">*</span> </label>
+                            <input class="input-form" type="text" name="subject" placeholder="sujet..." />
+                        </div>
+                        <div class="field-contact">
+                            <label class="form-label" for="message">Votre message<span class="red">*</span></label>
+                            <textarea class="textarea-form" name="message" cols="40" rows="5" placeholder="60 cartactères minimum..."></textarea>
+                        </div>
+                        <div class="content-button-form-contact">
+                            <button class="button-form" type="submit">envoyer </button>
+                        </div>
+
+                    </form>
+                    <div >
                     </div>
-                    <div class="field-contact">
-                        <label class="form-label" for="subject">Sujet <span class="red">*</span> </label>
-                        <input class="input-form" type="text" name="subject" placeholder="sujet..." />
-                    </div>
-                    <div class="field-contact">
-                        <label class="form-label" for="message">Votre message<span class="red">*</span></label>
-                        <textarea class="textarea-form" name="message" cols="40" rows="5" placeholder="60 cartactères minimum..."></textarea>
-                    </div>
-                    <div class="content-button-form-contact">
-                        <button class="button-form" type="submit">envoyer </button>
-                    </div>
-                </form>
+                </div>
+                <div class="content_img-contact2">
+                    <img class="img_contact2"src="<?php echo HOME_URL . 'assets/img/src/source/img-page-contact2'; ?>" alt="">
+
+                </div>
             </div>
         </section>
 
